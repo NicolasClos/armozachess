f90c7e832040cfd5c1683e5bbe0e1f18445bb592
NicolasClos <nicolasclos1@gmail.com>
2023-12-18 17:33:01 -0300

    Added SpeedInsights from Vercel

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import './globals.css'
 import Providers from './Providers'
 import Header from '@/components/header'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers><Header />{children}</Providers>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
